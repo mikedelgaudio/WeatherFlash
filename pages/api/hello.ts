95b4104120629faab5b0a81232639e2b91f65934
Mike DelGaudio <mdelgaud@stevens.edu>
2020-11-08 15:47:42 -0500

    using dotenv and testing fetching weather data

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const api = `http://api.openweathermap.org/data/2.5/weather?q=Hoboken&appid=${process.env.API_KEY}`;
+  const fetched = await fetch(api);
+  const json = await fetched.json();
+  res.statusCode = 200;
+  res.json(json);
+};
