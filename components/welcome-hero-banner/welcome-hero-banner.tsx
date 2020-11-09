9ec3e5f25c4cdb9b3b5e00e25f2478f09e19a9b8
Mike DelGaudio <mdelgaud@stevens.edu>
2020-11-08 21:30:57 -0500

    changing weather endpoint

--- components/welcome-hero-banner/welcome-hero-banner.tsx
+++ components/welcome-hero-banner/welcome-hero-banner.tsx
@@ -8,11 +8,11 @@ export default class WelcomeHeroBanner extends Component {
 
   public render() {
     return (
-      <div>
+      <div className="container">
         <h1 className={styles.title}>WeatherFlash</h1>
         <h2 className={styles.header}>Your weather update, in a flash!</h2>
         <h3>To get started, allow location in your browser or type a city ___</h3>
-        <label for="weather-location">
+        <label htmlFor="weather-location">
           Allow location or type city
           <input name="weather-location" placeholder="Allow location or type city"></input>
         </label>
