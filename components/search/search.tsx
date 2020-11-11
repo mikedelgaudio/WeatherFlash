dc907a4f08fe56d767195e63bf83c0a273b4dca4
Mike DelGaudio <mdelgaud@stevens.edu>
2020-11-11 18:17:32 -0500

    fixing form styling

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -6,24 +6,26 @@ import styles from "./search.module.scss";
 class Search extends Component {
   render() {
     return (
-      <form className={styles.search}>
-        <div className="input-group">
-          <label htmlFor="weather-lookup">Weather Lookup field</label>
-          <input
-            name="weather-lookup"
-            id="weather-lookup"
-            type="text"
-            className="form-control"
-            placeholder="Allow location or type city"
-          />
+      <div className={styles.searchForm}>
+        <form>
+          <div className="input-group">
+            <label htmlFor="weather-lookup">Allow location or type city</label>
+            <input
+              name="weather-lookup"
+              id="weather-lookup"
+              type="text"
+              className="form-control"
+              placeholder="Allow location or type city"
+            />
 
-          <div className="input-group-append">
-            <button className="btn btn-outline-dark">
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
+            <div className="input-group-append">
+              <button className="btn btn-outline-dark">
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
