e41f1615b78e3fcc0b62d16767a07b517f22a3b7
Mike DelGaudio <mdelgaud@stevens.edu>
2020-11-15 19:19:47 -0500

    changing location button click working on setting state

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -4,23 +4,44 @@ import { Component } from "react";
 import styles from "./search.module.scss";
 
 class Search extends Component {
-  test() {
+  constructor(props) {
+    super(props);
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.findLocation = this.findLocation.bind(this);
+  }
+
+  findLocation() {
     if ("geolocation" in navigator) {
-      console.log("HELLO");
+      console.log("Finding your location");
       navigator.geolocation.getCurrentPosition((position) => {
         console.log(position);
       });
     } else {
-      console.log("NO");
+      console.log("Could not find your location");
     }
   }
 
   handleSearch(e) {
-    console.warn("I HAVE BEEN CLICKED" + e);
+    const elementID = e.currentTarget.id;
+    console.log(elementID);
+    if (elementID === "location") {
+      //Grab Location
+      console.log("Requested Click on Location");
+      this.findLocation();
+    } else {
+      //Search City
+      console.log("Requested Click on City");
+      this.state.weatherLookup;
+    }
+  }
+
+  handleWeatherLookupChange(e) {
+    this.setState({ weatherLookup: e.target.value });
   }
 
   componentDidMount() {
-    window.addEventListener("load", this.test);
+    //window.addEventListener("load", this.test);
   }
 
   render() {
@@ -28,31 +49,35 @@ class Search extends Component {
       <div className={styles.searchForm}>
         <form>
           <div className="input-group">
-            <label htmlFor="weather-lookup">Allow location or type city</label>
+            <label htmlFor="weatherLookup">Allow location or type city</label>
             <input
-              name="weather-lookup"
-              id="weather-lookup"
+              name="weatherLookup"
+              id="weatherLookup"
               type="text"
               className="form-control"
               placeholder="Allow location or type city"
+              value={this.state.weatherLookup}
+              onChange={this.handleWeatherLookup}
             />
 
             <div className="input-group-append">
               <button
+                id="city"
                 type="button"
                 className="btn btn-outline-dark"
                 aria-label="Search"
-                onClick={this.handleSearch.bind(this)}
+                onClick={this.handleSearch}
               >
                 <FontAwesomeIcon icon={faSearch} />
               </button>
             </div>
             <div className="input-group-append">
               <button
+                id="location"
                 type="button"
                 className="btn btn-outline-dark"
                 aria-label="Find Location"
-                onClick={this.handleSearch.bind(this)}
+                onClick={this.handleSearch}
               >
                 <FontAwesomeIcon icon={faLocationArrow} />
               </button>
