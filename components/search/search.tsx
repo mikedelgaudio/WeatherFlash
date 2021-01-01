9de4e3957ef23dd98fde2a975b6cab630a389fae
Mike DelGaudio <mdelgaud@stevens.edu>
2021-01-01 14:12:39 -0500

    remove old autocomplete package; add node-sass instead; fix search border

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -3,66 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Component } from "react";
 import styles from "./search.module.scss";
 
-const cities = [{ name: "New York" }, { name: "Cupertino" }];
-
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = (value) => {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
-
-  return inputLength === 0
-    ? []
-    : cities.filter((lang) => lang.name.toLowerCase().slice(0, inputLength) === inputValue);
-};
-
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = (suggestion) => suggestion.name;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = (suggestion) => <div>{suggestion.name}</div>;
-
 class Search extends Component<any, any> {
   constructor(props) {
     super(props);
-    this.state = {
-      value: "",
-      suggestions: [],
-    };
   }
-
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
-    });
-  };
-
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: getSuggestions(value),
-    });
-  };
-
-  // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
-  };
-
   render() {
-    const { value, suggestions } = this.state;
-
-    // Autosuggest will pass through all these props to the input.
-    const inputProps = {
-      placeholder: this.props.placeholder,
-      value,
-      onChange: this.props.handleUserInput,
-    };
-
     return (
       <div className={styles.searchForm}>
         <form onSubmit={this.props.handleSearch} id="weatherLookupForm">
@@ -76,18 +21,6 @@ class Search extends Component<any, any> {
               placeholder={this.props.placeholder}
               onChange={this.props.handleUserInput}
             />
-            {/* <Autosuggest
-              name="weatherLookup"
-              type="text"
-              id="weatherLookupField"
-              className="form-control"
-              suggestions={suggestions}
-              onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-              onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-              getSuggestionValue={getSuggestionValue}
-              renderSuggestion={renderSuggestion}
-              inputProps={inputProps}
-            /> */}
 
             <div className="input-group-append">
               <button
