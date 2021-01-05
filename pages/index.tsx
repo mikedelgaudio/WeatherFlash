9e1573893ad49d2bad497c4e515c13cffcfaf79a
Mike DelGaudio <mdelgaud@stevens.edu>
2021-01-04 20:21:41 -0500

    update GA

--- pages/index.tsx
+++ pages/index.tsx
@@ -415,12 +415,13 @@ export default class Home extends Component<any, any> {
 
 /**
  * <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-8V07L1WK67"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-H7JJPMJC5Q"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-8V07L1WK67');
+  gtag('config', 'G-H7JJPMJC5Q');
 </script>
  */
