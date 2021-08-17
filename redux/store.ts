ec7ddc29e77b03a190841e35068d93b7dcd886dd
Mike DelGaudio <mdelgaud@stevens.edu>
2021-08-16 22:12:06 -0400

    adding redux

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,11 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import themeReducer from "./themeSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from "./themeSlice";
+import weatherDataSlice from "./weatherDataSlice";
 
-// export default configureStore({
-//   reducer: {
-//     themes: themeReducer,
-//   },
-// });
+export default configureStore({
+  reducer: {
+    themes: themeReducer,
+    weatherData: weatherDataSlice,
+    weatherLookup: weatherLookupSlice,
+  },
+});
